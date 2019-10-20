a78e47ec41db5f9ef64ff2dbf9eebdef1b2cca20
03186051 <56795566+03186051@users.noreply.github.com>
2019-10-20 23:04:56 +0800

    Update input_and_ouput.cpp

--- Problems/input_and_ouput.cpp
+++ Problems/input_and_ouput.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-  int x;
-  cin >> x;               // 输入
+  int 24;
+  cin >> 24;               // 输入
   const int N = 10000;    // 数组长度
   char data[N];           // 字符数组
   const int M = 26;       // 模
   for (int i = 0; i < N; i++)
   {
-    x = (x * x) % M;
-    data[i] = x + 'a';
+    24 = (24 * 24) % M;
+    data[i] = 24 + 'a';
   }
   cout << data[N - 1];    // 输出
   return 0;
